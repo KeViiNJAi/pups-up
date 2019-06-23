debcbd1a7c70291191fb7f07f0586291cce093a7
Kevin Lam <find.keviin@gmail.com>
2019-06-23 17:38:01 -0400

    successfully query and render firebase data to component

--- components/ProfilePage.js
+++ components/ProfilePage.js
@@ -2,34 +2,47 @@ import React, { Component } from 'react';
 import {
   Dimensions,
   Image,
-  Platform,
   ScrollView,
   StyleSheet,
   Text,
-  TouchableOpacity,
   View
 } from 'react-native';
+import firebase from 'firebase';
 
 export default class ProfilePage extends Component {
+  //new syntax to avoid having to use constructor function
+  state = {}
+
+  componentDidMount() {
+    firebase
+      .database()
+      .ref('/')
+      .once('value')
+      .then((snapshot) => {
+        this.setState(snapshot.val());
+        console.log(snapshot.val())
+      });
+    }
+
   render() {
+    if (!this.state.users){
+      return null
+    }
     return (
-      <View
-      style={styles.container}
-      >
+      <View style={styles.container}>
         <ScrollView>
           <View style={styles.container}>
             <Image
-              // style={{ width: 300, height: 300 }}
               source={require('../assets/images/theo.jpg')}
               style={styles.profileImg}
             />
 
-            <Text style={styles.userInfoText}>Username: Theo.theSamoyed</Text>
-            <Text style={styles.userInfoText}>Owner: Kevin</Text>
-            <Text style={styles.userInfoText}>Name: Theodore</Text>
-            <Text style={styles.userInfoText}>Breed: Samoyed</Text>
-            <Text style={styles.userInfoText}>Home: New York, NY</Text>
-            <Text style={styles.userInfoText}>Siblings: Avon, Quincy...</Text>
+            <Text style={styles.userInfoText}>Username: {this.state.users.user1.username}</Text>
+            <Text style={styles.userInfoText}>Owner: {this.state.users.user1.name}</Text>
+            <Text style={styles.userInfoText}>Name: {this.state.pets.pet1.name}</Text>
+            <Text style={styles.userInfoText}>Breed: {this.state.pets.pet1.breed}</Text>
+            <Text style={styles.userInfoText}>Home: {this.state.users.user1.city}</Text>
+            <Text style={styles.userInfoText}>Friends: {this.state.pets.pet2.name}</Text>
           </View>
         </ScrollView>
       </View>
@@ -55,6 +68,6 @@ const styles = StyleSheet.create({
   userInfoText: {
     fontSize: 17,
     color: 'rgba(96,100,109, 1)',
-    textAlign: 'center',
+    textAlign: 'center'
   }
 });
